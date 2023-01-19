933ec5553dfd813568fe54fc68102720b3a1738b
Maciej Wódke <maciej@mwpro.pl>
2023-01-19 17:22:53 +0100

    Init project

---
+++ Mw.StreamDeck/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenMacroBoard.SDK;
+using StreamDeckSharp;
+
+var red = KeyBitmap.Create.FromRgb(237, 41, 57);
+var white = KeyBitmap.Create.FromRgb(255, 255, 255);
+var rowColors = new KeyBitmap[] { red, white, red };
+
+//Open the Stream Deck device
+using (var deck = StreamDeck.OpenDevice())
+{
+    deck.SetBrightness(100);
+
+    //Send the bitmap informaton to the device
+    for (int i = 0; i < deck.Keys.Count; i++)
+        deck.SetKeyBitmap(i, rowColors[i / 5]);
+
+    Console.ReadKey();
+}
