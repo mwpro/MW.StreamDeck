db28f465e2aa5bcb4c3b83d56d4a0aab10c6b472
Maciej Wódke <maciej@mwpro.pl>
2023-01-19 19:53:28 +0100

    Blinking alert with reset

--- Mw.StreamDeck/HelloWorld.cs
+++ Mw.StreamDeck/HelloWorld.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
 using System.Drawing.Imaging;
 using SharpDeck;
 using SharpDeck.Events.Received;
@@ -6,53 +7,80 @@ using SharpDeck.Events.Received;
 namespace MW.StreamDeck;
 
 [StreamDeckAction("com.mwpro.streamdeck.helloworld")]
+[SuppressMessage("Interoperability", "CA1416")]
 public class HelloWorld : StreamDeckAction
 {
     private int _counter = 0;
     private PeriodicTimer _timer;
+    private PeriodicTimer _blinker = null;
 
     public HelloWorld()
     {
-        _timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
-        DoWorkAsync();
+        _timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+        EmulateAlertsWithTimer();
     }
 
     protected override void OnInit(ActionEventArgs<AppearancePayload> args)
     {
-        SetImageAsync(PrepareBackground(Color.Green, "OK"));
+        Reset();
         base.OnInit(args);
     }
 
-    private async Task DoWorkAsync()
+    protected override async Task OnKeyDown(ActionEventArgs<KeyPayload> args)
+    {
+        await Reset();
+    }
+
+    private async Task EmulateAlertsWithTimer()
     {
         while (await _timer.WaitForNextTickAsync())
         {
             _counter++;
             var isDanger = _counter >= 3;
-            
-            SetImageAsync(PrepareBackground(isDanger ? Color.Red : Color.Yellow, isDanger ? "ALARM" : "WARN"));
+
+            SetAlert(isDanger);
         }
     }
 
-    private string PrepareBackground(Color color, string text)
+    private void SetAlert(bool isDanger)
+    {
+        _blinker = new PeriodicTimer(TimeSpan.FromSeconds(1));
+        DoBlink(isDanger ? Color.Red : Color.Yellow, 
+            isDanger ? Color.White : Color.Black,
+            isDanger ? "ALARM" : "WARN");
+    }
+
+    private async Task DoBlink(Color backgroundColor, Color textColor, string text)
+    {
+        var previousStateWasColor = false;
+        while (await _blinker.WaitForNextTickAsync())
+        {
+            SetImageAsync(PrepareBackground(previousStateWasColor ? Color.Black : backgroundColor, 
+                previousStateWasColor ? Color.White : textColor, 
+                text));
+            previousStateWasColor = !previousStateWasColor;
+        }
+    }
+
+    private async Task Reset()
+    {
+        _counter = 0;
+        _blinker?.Dispose();
+        await SetImageAsync(PrepareBackground(Color.Green, Color.White, "OK"));
+    }
+
+    private string PrepareBackground(Color backgroundColor, Color textColor, string text)
     {
         var ms = new MemoryStream();
         var bitmap = new Bitmap(72, 72);
-        var img = System.Drawing.Graphics.FromImage(bitmap);
+        var img = Graphics.FromImage(bitmap);
         var rect = new Rectangle(0, 0, 72, 72);
-        img.DrawRectangle(new Pen(color), rect);
-        img.FillRectangle(new SolidBrush(color), rect);
-        img.DrawString(text, new Font(FontFamily.GenericSansSerif, 15), Brushes.Black, 0, (72-15)/2);
+        img.DrawRectangle(new Pen(backgroundColor), rect);
+        img.FillRectangle(new SolidBrush(backgroundColor), rect);
+        img.DrawString(text, new Font(FontFamily.GenericSansSerif, 15), new SolidBrush(textColor), 0, (72-15)/2);
         bitmap.Save(ms, ImageFormat.Png);
-        byte[] byteImage = ms.ToArray();
+        var byteImage = ms.ToArray();
         var str = Convert.ToBase64String(byteImage);
-        return "data:image/png;base64," + str;
-    }
-    
-    // Methods can be overriden to intercept events received from the Stream Deck.
-    protected override async Task OnKeyDown(ActionEventArgs<KeyPayload> args)
-    {
-        _counter = 0;
-        await SetImageAsync(PrepareBackground(Color.Green, "OK"));
+        return $"data:image/png;base64,{str}";
     }
 }
